b19338a854d982a6ce4eeddc283f854c2e72f6e0
dca-davi <macbook@MacBooks-MBP-2.home>
2024-12-02 14:49:46 -0500

    fixed erro fixture browser not found

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,29 +17,29 @@ def config(scope='session'):
     assert isinstance(config['implicit_wait'], int)
     assert config['implicit_wait'] > 0
 
-    #return config so it can be used
+    # return config so it can be used
     return config    
     
-    @pytest.fixture
-    def browser(config):
+@pytest.fixture
+def browser(config):
         
-        # Initialize  the WebDrive instance
-        if config['browser'] == 'Firefox':
-            b = selenium.webdriver.Firefox()
-        elif config['browser'] == 'Chrome':
-            b = selenium.webdriver.Chrome()
-        elif config['browser'] == 'Headless Chrome':
-            opts = selenium.webdriver.ChromeOptions()
-            opts.add_argument('headless')
-            b = selenium.webdriver.Chrome(options=opts)
-        else:
-            raise Exception(f'Browser "{config["browser"]}" is not supported')
+    # Initialize  the WebDrive instance
+    if config['browser'] == 'Firefox':
+        b = selenium.webdriver.Firefox()
+    elif config['browser'] == 'Chrome':
+        b = selenium.webdriver.Chrome()
+    elif config['browser'] == 'Headless Chrome':
+        opts = selenium.webdriver.ChromeOptions()
+        opts.add_argument('headless')
+        b = selenium.webdriver.Chrome(options=opts)
+    else:
+        raise Exception(f'Browser "{config["browser"]}" is not supported')
 
-        # Make its calls wait for elements to apper
-        b.implicitly_wait(config['implicit_wait'])
+    # Make its calls wait for elements to apper
+    b.implicitly_wait(config['implicit_wait'])
 
-        # Return the webdrive  instance for the setup
-        yield b
+    # Return the webdrive  instance for the setup
+    yield b
 
-        # Quit the webdrive instance for the cleanup
-        b.quit()       
+    # Quit the webdrive instance for the cleanup
+    b.quit()       
